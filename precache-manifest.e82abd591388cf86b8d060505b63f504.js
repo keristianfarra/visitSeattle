723864e6cf27c45d6a2b3d0403052bf59b22b509
Keristian Farra <farrak@uw.edu>
2019-02-26 18:41:25 -0800

    Updates

---
+++ precache-manifest.e82abd591388cf86b8d060505b63f504.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "427cc8e69bb5df8f3a5c",
+    "url": "/visitSeattle/static/js/runtime~main.427cc8e6.js"
+  },
+  {
+    "revision": "8ea1227e9461303676ad",
+    "url": "/visitSeattle/static/js/main.8ea1227e.chunk.js"
+  },
+  {
+    "revision": "58204fde93b5f6bb84fc",
+    "url": "/visitSeattle/static/js/2.58204fde.chunk.js"
+  },
+  {
+    "revision": "8ea1227e9461303676ad",
+    "url": "/visitSeattle/static/css/main.7109da53.chunk.css"
+  },
+  {
+    "revision": "8fefd758e1299f4038936074d2279175",
+    "url": "/visitSeattle/index.html"
+  }
+];
