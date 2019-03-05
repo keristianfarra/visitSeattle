dbd1ce169c18961b65d15a5d50799ae9a5bf8f83
Keristian Farra <farrak@uw.edu>
2019-03-04 21:18:10 -0800

    Fixed some issues such as added .gitignore

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -15,28 +15,16 @@ export default class SimpleSlider extends React.Component {
     
       <Slider {...settings}>
       <div>
-          <img src="/home.jpg" alt="A picture of Seattle."/>
+          <img src="/home.jpg" alt="Seattle Skyline"/>
         </div>
         <div>
-          <img src="/FremontTroll.jpg" alt="A picture of Seattle."/>
+          <img src="/FremontTroll.jpg" alt="The Fremont Troll in Seattle."/>
         </div>
         <div>
-          <img src="/PikePlaceMarket.jpg" alt="A picture of Seattle."/>
+          <img src="/PikePlaceMarket.jpg" alt="Pike Place Market in Downtown Seattle."/>
         </div>
         <div>
-          <img src="/GumWall.jpg" alt="A picture of Seattle."/>
-    </div>
-    <div>
-          <img src="/home.jpg" alt="A picture of Seattle."/>
-        </div>
-        <div>
-          <img src="/FremontTroll.jpg" alt="A picture of Seattle."/>
-        </div>
-        <div>
-          <img src="/PikePlaceMarket.jpg" alt="A picture of Seattle."/>
-        </div>
-        <div>
-          <img src="/GumWall.jpg" alt="A picture of Seattle."/>
+          <img src="/GumWall.jpg" alt="The Famous Gum Wall."/>
     </div>
       </Slider>
     );
