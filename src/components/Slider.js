ca7b219fd713f26b3b35b7c720cfc897e615cb6e
Keristian Farra <farrak@uw.edu>
2019-03-04 22:59:42 -0800

    Fixed package.json

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,15 +2,7 @@ import React from 'react';
 import Slider from "react-slick";
 
 export default class SimpleSlider extends React.Component {
-/*   render() {
-    var settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    };
- */
+
   render() {
     var settings = {
       dots: true,
@@ -23,16 +15,18 @@ export default class SimpleSlider extends React.Component {
     
       <Slider {...settings}>
       <div>
-          <img src="/home.jpg" alt="A picture of Seattle."/>
+          <img src="/home.jpg" alt="Seattle Skyline"/>
         </div>
         <div>
-          <img src="/home.jpg" alt="A picture of Seattle."/>
+          <img src="/FremontTroll.jpg" alt="The Fremont Troll in Seattle."/>
         </div>
         <div>
-          <img src="/home.jpg" alt="A picture of Seattle."/>
+          <img src="/PikePlaceMarket.jpg" alt="Pike Place Market in Downtown Seattle."/>
+        </div>
+        <div>
+          <img src="/GumWall.jpg" alt="The Famous Gum Wall."/>
     </div>
       </Slider>
     );
   }
 } 
-
