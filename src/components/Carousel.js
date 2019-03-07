f0532ec27b03da3d421b0371847010aba9fa4b93
Keristian Farra <farrak@uw.edu>
2019-03-06 17:54:18 -0800

    Fixed the issue with AppBar and the React Route path to work. Will remove Slider and use carousel buttons instead

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,1 +1,41 @@
-import {Arrow} from '@material-ui/icons';
+import {ArrowForward,ArrowBack} from '@material-ui/icons';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+/* import IconButton from '@material-ui/core/IconButton';
+import Icon from '@material-ui/core/Icon'; */
+/* import LeftArrow from './LeftArrow.png';
+import RightArrow from './RightArrow.png'; */
+
+const styles = theme => ({
+  fab: {
+    margin: theme.spacing.unit,
+  },
+  extendedIcon: {
+    marginRight: theme.spacing.unit,
+  },
+});
+function FloatingActionButtons(props) {
+    const { classes } = props;
+    return (
+      <div>
+        <Fab color="primary" aria-label="Back" className={classes.fab}>
+          <ArrowForward/>
+        </Fab>
+        <Fab color="primary" aria-label="Back" className={classes.fab}>
+          <ArrowBack/>
+        </Fab>
+        {/* <Fab color="primary" aria-label="Forward" className={classes.fab}>
+          <Icon>edit_icon</Icon>
+        </Fab> */}
+        
+      </div>
+    );
+  }
+  
+  FloatingActionButtons.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  
+  export default withStyles(styles)(FloatingActionButtons);
